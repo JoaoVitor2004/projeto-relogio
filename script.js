6d3dc48d6dec70615dbf4d8e2ea8654cfb7b4d56
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-03-16 11:00:22 -0300

    adicionei os arquivos do projeto

---
+++ script.js
@@ -0,0 +1,28 @@
+const relogio = () => {
+    const resultado = document.querySelector('#relogio')
+    const data = new Date()
+    const horas = String(data.getHours()).padStart(2,'0')
+    const minutos = String(data.getMinutes()).padStart(2,'0')
+    const segundos = String(data.getSeconds()).padStart(2,'0')
+    resultado.innerHTML = `${horas}:${minutos}:${segundos}`
+}
+const intervalo = setInterval(relogio, 1000)
+
+const promesa = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        let soma = 5 + 5
+        if (soma === 10) {
+            resolve('Tudo certo, valor encontrado')
+        } else {
+            reject('Erro, valor não encontrado')
+        }
+    }, 2000)
+})
+
+promesa.then((resolvido) => {
+    console.log(resolvido)
+}).catch((erro) => {
+    console.log(erro)
+}).finally((final) => {
+    console.log('Fim da aplicação')
+})
