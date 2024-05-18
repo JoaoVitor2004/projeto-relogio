bf11c3add63d6652e8171dfc0d370e21805c9a27
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-05-18 14:30:21 -0300

    novas alterações

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-const resultado = document.querySelector('#relogio')
+
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
 const relogio = () => {
     const data = new Date()
-    const horas = String(data.getHours()).padStart(2,'0')
-    const minutos = String(data.getMinutes()).padStart(2,'0')
-    const segundos = String(data.getSeconds()).padStart(2,'0')
-    resultado.innerHTML = `${horas}:${minutos}:${segundos}`
+    horas.innerHTML = String(data.getHours()).padStart(2,'0')
+    minutos.innerHTML = String(data.getMinutes()).padStart(2,'0')
+    segundos.innerHTML = String(data.getSeconds()).padStart(2,'0')
 }
 const intervalo = setInterval(relogio, 1000)
 
-const promesa = new Promise((resolve,reject) => {
-    setTimeout(() => {
-        let soma = 5 + 5
-        if (soma === 10) {
-            resolve('Tudo certo, valor encontrado')
-        } else {
-            reject('Erro, valor não encontrado')
-        }
-    }, 2000)
-})
 
-promesa.then((resolvido) => {
-    console.log(resolvido)
-}).catch((erro) => {
-    console.log(erro)
-}).finally((final) => {
-    console.log('Fim da aplicação')
-})
+
+// const promesa = new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//         let soma = 5 + 5
+//         if (soma === 10) {
+//             resolve('Tudo certo, valor encontrado')
+//         } else {
+//             reject('Erro, valor não encontrado')
+//         }
+//     }, 2000)
+// })
+
+// promesa.then((resolvido) => {
+//     console.log(resolvido)
+// }).catch((erro) => {
+//     console.log(erro)
+// }).finally((final) => {
+//     console.log('Fim da aplicação')
+// })
