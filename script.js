04e9842fa10fb3a84a8f1f775452f0bc893de752
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-07-14 16:15:58 -0300

    Adicionar novas alterações

--- script.js
+++ script.js
@@ -2,10 +2,24 @@
 const horas = document.getElementById('horas')
 const minutos = document.getElementById('minutos')
 const segundos = document.getElementById('segundos')
-const relogio = () => {
+const dia = document.getElementById('dia')
+const mês = document.getElementById('mes')
+const ano = document.getElementById('ano')
+
+const dataAtual = () => {
     const data = new Date()
-    horas.innerHTML = String(data.getHours()).padStart(2,'0')
-    minutos.innerHTML = String(data.getMinutes()).padStart(2,'0')
-    segundos.innerHTML = String(data.getSeconds()).padStart(2,'0')
+    dia.innerHTML = `${String(data.getDate()).padStart(2,'0')} do`
+    mês.innerHTML = `${String(data.getMonth() + 1).padStart(2,'0')} de`
+    ano.innerHTML = data.getFullYear()
 }
-const intervalo = setInterval(relogio, 1000)
+
+dataAtual()
+
+function horario() {
+    const data = new Date()
+    horas.innerHTML = `${String(data.getHours()).padStart(2,'0')} :`
+    minutos.innerHTML =`${ String(data.getMinutes()).padStart(2,'0')} :`
+    segundos.innerHTML = `${String(data.getSeconds()).padStart(2,'0')}`
+}
+
+const intervalo = setInterval(horario, 1000)
